9bbde0b8b44b62f1a59460b76bbef2d8211ef55f
jonniek01 <jontejonhn@gmail.com>
2022-06-07 21:07:29 +0300

    first commit

---
+++ src/components/TodoForm.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react'
+import { useTodoContext } from '../Providers/TodoProvider';
+
+export const TodoForm = () => {
+    const {getNumberOfTodoItems, addTodo}=useTodoContext();
+    const [todoItem, setTodoItem]=useState("")
+   const handleOnSubmit = (e) =>{
+      e.preventDefault()
+      if(!todoItem){
+        return
+      }
+      addTodo(todoItem)
+      setTodoItem("")
+    }
+  return (
+   <form onSubmit={handleOnSubmit} >
+       <h3>Number of todo items: {getNumberOfTodoItems()}</h3>
+       <input type="text" value={todoItem} onChange={e=>setTodoItem(e.target.value)} ></input>
+       <button type='submit' >Submit</button>
+   </form>
+  )
+}
